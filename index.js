84bf310c1b88ffd79944681c4673d808b1a193ea
marwa <mds08@mail.aub.edu>
2022-08-03 00:01:03 +0300

    completed assignment

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ document.addEventListener("keydown", function(e) {
 function moveDodgerRight() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
-    if (left > 0) {
+    if (left < 359) {
         dodger.style.left = `${left + 1}px`;
     }
 }
 document.addEventListener("keydown", function(e) {
-    if (e.key === "ArrowRight") {
+    if (e.key === "ArrowRight" ) {
         moveDodgerRight();
     }
 })
